3fd486261033723e3885d3d55d68907fd244abd8
woong9149 <sewoong0409@naver.com>
2020-10-19 23:27:57 +0900

    app.js 생성
    app.js 생성

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const morgan = require('morgan');
+const session = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
+
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
+
+const authRouter = require('./routes/auth');
+const indexRouter = require('./routes');
+
+const app = express();
+sequelize.sync();
+passportConfig(passport);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 8002);
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.join());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+
+
+
+
+
+
+
