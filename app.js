61d89720eda2b7d388bcaff0d33ad52deaa7622f
woong9149 <sewoong0409@naver.com>
2020-10-21 16:08:06 +0900

    app.js 추가 3
    app.js 추가 3

--- app.js
+++ app.js
@@ -42,7 +42,22 @@ app.use(passport.session());
 app.use('/auth', authRouter);
 app.use('/', indexRouter);
 
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
+app.use((err, req, res) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), ' 번 포트에서 대기 중');
+})
 
 
 
